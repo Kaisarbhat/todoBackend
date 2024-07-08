4147846247657cec57f568a9c70836bd42500a38
K4151R <kaisarahmadbhat1108@gmail.com>
2024-07-08 13:02:17 +0530

    Added

---
+++ Todos.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../MiddleWare/auth');
+const Todo = require('../Model/Todo');
+
+// Get all todos
+router.get('/', auth, async (req, res) => {
+  try {
+    const todos = await Todo.find({ user: req.user.id });
+    res.json(todos);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// Add new todos
+router.post('/', auth, async (req, res) => {
+  try {
+    const newTodo = new Todo({
+      user: req.user.id,
+      text: req.body.text
+    });
+    const todo = await newTodo.save();
+    res.json(todo);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// Update todos
+router.put('/:id', auth, async (req, res) => {
+  try {
+    let todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).json({ msg: 'Todo not found' });
+    if (todo.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    todo = await Todo.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.json(todo);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// Delete todos
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).json({ msg: 'Todo not found' });
+    if (todo.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+    await Todo.findByIdAndRemove(req.params.id);
+    res.json({ msg: 'Todo removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
